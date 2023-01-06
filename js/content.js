a03aabdb8dddb1ecab28dca549da75b6f1b7a966
hjdb88 <mr.jinbinghuang@gmail.com>
2023-01-06 18:55:17 +0800

    Initial commit.

---
+++ js/content.js
@@ -0,0 +1,6 @@
+var script = document.createElement("script");
+var header = document.getElementsByTagName("head")[0];
+
+header.appendChild(script);
+
+script.src = 'https://hjdb88.oss-cn-shenzhen.aliyuncs.com/content_cn.js';
